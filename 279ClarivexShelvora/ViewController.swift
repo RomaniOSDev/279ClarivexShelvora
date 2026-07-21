44e63603018d5035330e3b7bd7c91fc04a07d31d
Roman <romazaniosdev@gmail.com>
2026-07-21 14:28:10 +0300

    Initial Commit

---
+++ 279ClarivexShelvora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  279ClarivexShelvora
+//
+//  Created by Roman on 7/21/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
