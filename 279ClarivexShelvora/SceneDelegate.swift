757641f7b900b90152852877cd646c31839cb430
Roman <romazaniosdev@gmail.com>
2026-08-05 01:40:42 +0300

    add view

--- 279ClarivexShelvora/SceneDelegate.swift
+++ 279ClarivexShelvora/SceneDelegate.swift
@@ -11,12 +11,13 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    private var launchFlowResolver: LaunchFlowResolver?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else {return}
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIHostingController(rootView: ContentView())
+        launchFlowResolver = LaunchFlowResolver(window: window)
+        window?.rootViewController = launchFlowResolver?.resolveEntryViewController()
         window?.makeKeyAndVisible()
     }
 
@@ -50,4 +51,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
 }
-
